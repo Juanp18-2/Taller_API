9b55b85c9b535e7c1024180406820b1b8eb6b955
juanp <juanp@DESKTOP-BV4N6DG>
2025-05-14 16:28:55 -0500

    Correcion y creacion de Config.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const sequelize = require("./config/database");
-const User = require("./models/user.model");
-const Auth = require("./models/auth.model");
+const app = require("./app/app");
 
-await sequelize.sync({ alter: true });
-
-const app = express();
-require("dotenv").config();
-
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => res.send("API funcionando correctamente"));
-
-app.listen(PORT, async () => {
-    try {
-        await sequelize.authenticate();
-        console.log("Conexión con base de datos exitosa");
-    } catch (error) {
-    console.error("Error al conectar con la base de datos:", error);
-    }
-    console.log("Servidor escuchando en http://localhost:${PORT}");
+const port = app.listen(app.get("port"), () => {
+    console.log("Servidor escuchando en el puerto", app.get("port"));
 });
